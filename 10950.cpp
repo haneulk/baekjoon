8ada2ed11d0ea3a38e140153f405924c443a24ed
haneulk <hnkim362@naver.com>
2018-02-17 11:43:42 +0900

    A+B-3

--- 10950.cpp
+++ 10950.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main() {
-	int T, A, B;
-	int a[1000][2] = {};
+	int T, A, B;	
 	scanf("%d", &T);
-	for (int i = 0; i < T; i++) {
-		scanf("%d %d", &a[i][0], &a[i][1]);
-		printf("%d\n", a[i][0] + a[i][1]);
+	while (T--) {	//T개수를 모를때 이런식으로 구현하기
+		scanf("%d %d", &A, &B);
+		printf("%d\n", A + B);
 	}
-
 	return 0;
 }
