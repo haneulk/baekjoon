9d36db76f97f84b4454df55029e445476e46ae92
haneulk <hnkim362@naver.com>
2018-02-22 11:38:27 +0900

    외판원 순회2 - 시간복잡도 O(N*N!) -> O(N!)로 줄임

--- 10971.cpp
+++ 10971.cpp
@@ -30,7 +30,15 @@ int main() {
 		if (key != 1)
 			result = min(result, sum);
 
-	} while (next_permutation(p, p + N));
+	} while (next_permutation(p + 1, p + N));
+	/*
+	p가 아닌 p+1부터 시작하면 맨 앞에가 고정되어있고 2번자리수만 바뀌면서 순열이 적용됨
+	이 문제 자체가 마지막 지점에서 바로 처음출발지로 돌아오기때문에
+	1234 2341 3412 4123 이게 다 똑같은 경로이자 거리가 됨
+	따라서 앞에 1을 고정을 시킨 후 뒤에만 바꿔줘도 시간 복잡도가
+	O(N*N!)에서 O(N!)로 바뀜
+	실제로 채점 결과 52MS가 걸렸던것이 4MS로 바뀜!!
+	*/
 	printf("%d\n", result);
 	return 0;
 }
